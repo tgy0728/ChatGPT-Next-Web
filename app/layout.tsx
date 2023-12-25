fbe2bfef946822b6fabdc1dc00dc5a30b2428acd
tgy0728 <151910804+tgy0728@users.noreply.github.com>
2023-12-25 15:09:29 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "AI-Assistant",
+  description: "Your personal AI-Chat Assistant Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "AI-Assistant",
     statusBarStyle: "default",
   },
 };
